5ca452a0cb7cc48190e9f0cbca573427c2f58d7f
bernardo300 <bernardolopes.luz@hotmail.com>
2021-04-26 16:06:42 -0300

    save Personservice

--- src/main/java/digital/one/PesonalApi/controller/PersonController.java
+++ src/main/java/digital/one/PesonalApi/controller/PersonController.java
@@ -3,27 +3,23 @@ package digital.one.PesonalApi.controller;
 import digital.one.PesonalApi.dto.MessageResponseDTO;
 import digital.one.PesonalApi.entity.Person;
 import digital.one.PesonalApi.repository.PersonRepository;
+import digital.one.PesonalApi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
+    private PersonService personService;
 
-    PersonRepository personRepository;
-
-    @Autowired
-    public PersonController(PersonRepository personRepository){
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
-
     @PostMapping
-    public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person personSave = this.personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Create person com id " + personSave.getId())
-                .build();
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        return this.personService.createPerson(person);
     }
 }
---
+++ src/main/java/digital/one/PesonalApi/service/PersonService.java
@@ -0,0 +1,27 @@
+package digital.one.PesonalApi.service;
+
+import digital.one.PesonalApi.dto.MessageResponseDTO;
+import digital.one.PesonalApi.entity.Person;
+import digital.one.PesonalApi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+    PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person){
+        Person personSave = this.personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Create person com id " + personSave.getId())
+                .build();
+    }
+}
