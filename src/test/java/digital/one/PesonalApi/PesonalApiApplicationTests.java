6e166e0f3b402a86bb19da303c983a4ce00bfb6e
bernardo300 <bernardolopes.luz@hotmail.com>
2021-04-26 09:03:34 -0300

    first commit

---
+++ src/test/java/digital/one/PesonalApi/PesonalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.one.PesonalApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesonalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
