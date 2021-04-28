49f9aa73d21fd4553e86762643f41065bf17a36a
bernardo300 <bernardolopes.luz@hotmail.com>
2021-04-28 13:44:45 -0300

    atualizacao pacote tdo.

---
+++ src/main/java/digital/one/PesonalApi/dto/request/PersonDTO.java
@@ -0,0 +1,40 @@
+package digital.one.PesonalApi.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDTO {
+
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 2, max = 30)
+    private String firstName;
+
+    @NotEmpty
+    @Size(min = 2, max = 30)
+    private String lastName;
+
+    @NotEmpty
+    @Size(min = 11, max = 11)
+    private String cpf;
+
+    private String birthDate;
+
+    @Valid
+    @NotEmpty
+    private List<PhoneDTO> phones;
+}
