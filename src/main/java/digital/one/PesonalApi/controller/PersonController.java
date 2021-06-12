9e18baf4097a96729f576c66dfbedea71babeabc
bernardo300 <bernardolopes.luz@hotmail.com>
2021-06-12 19:42:50 -0300

    refatorandoconstrutor

--- src/main/java/digital/one/PesonalApi/controller/PersonController.java
+++ src/main/java/digital/one/PesonalApi/controller/PersonController.java
@@ -4,6 +4,8 @@ import digital.one.PesonalApi.dto.MessageResponseDTO;
 import digital.one.PesonalApi.dto.request.PersonDTO;
 import digital.one.PesonalApi.exception.PersonNotFoundException;
 import digital.one.PesonalApi.service.PersonService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,12 +14,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
-    private PersonService personService;
+    private final PersonService personService;
 
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
+    //public PersonController(PersonService personService) {
+    //    this.personService = personService;
+    //}
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
